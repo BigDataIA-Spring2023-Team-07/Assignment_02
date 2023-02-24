451e4c4ddf3ec012cd8051eb4bc3a889151e17e1
Shubham Goyal <goyal.shubh15@gmail.com>
2023-02-24 17:18:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -168,7 +168,69 @@ def test_nexrad_file_retrival_main():
     url = response.json()['Response']
     assert url == "https://noaa-nexrad-level2.s3.amazonaws.com/2003/07/17/KBOX/KBOX20030717_014732.gz"
 
+def test_goes_file_retrival_main():
 
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L1b-RadC-M6C01_G18_s20230020101172_e20230020103548_c20230020103594.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L1b-RadC/2023/002/01/OR_ABI-L1b-RadC-M6C01_G18_s20230020101172_e20230020103548_c20230020103594.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ACMM1-M6_G18_s20230090504262_e20230090504319_c20230090505026.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ACMM/2023/009/05/OR_ABI-L2-ACMM1-M6_G18_s20230090504262_e20230090504319_c20230090505026.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ACTPM1-M6_G18_s20230090408262_e20230090408319_c20230090409174.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ACTPM/2023/009/04/OR_ABI-L2-ACTPM1-M6_G18_s20230090408262_e20230090408319_c20230090409174.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-DSIM1-M6_G18_s20230110608251_e20230110608308_c20230110609126.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-DSIM/2023/011/06/OR_ABI-L2-DSIM1-M6_G18_s20230110608251_e20230110608308_c20230110609126.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ACHTM1-M6_G18_s20223560805242_e20223560805300_c20223560806526.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ACHTM/2022/356/08/OR_ABI-L2-ACHTM1-M6_G18_s20223560805242_e20223560805300_c20223560806526.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-BRFF-M6_G18_s20223150230207_e20223150239515_c20223150241087.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-BRFF/2022/315/02/OR_ABI-L2-BRFF-M6_G18_s20223150230207_e20223150239515_c20223150241087.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ADPM2-M6_G18_s20230061310557_e20230061311015_c20230061311402.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ADPM/2023/006/13/OR_ABI-L2-ADPM2-M6_G18_s20230061310557_e20230061311015_c20230061311402.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L1b-RadM1-M6C01_G18_s20230030201252_e20230030201311_c20230030201340.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L1b-RadM/2023/003/02/OR_ABI-L1b-RadM1-M6C01_G18_s20230030201252_e20230030201311_c20230030201340.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ACHTF-M6_G18_s20223532240210_e20223532249518_c20223532252164.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ACHTF/2022/353/22/OR_ABI-L2-ACHTF-M6_G18_s20223532240210_e20223532249518_c20223532252164.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-DSRC-M6_G18_s20223180501179_e20223180503552_c20223180508262.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-DSRC/2022/318/05/OR_ABI-L2-DSRC-M6_G18_s20223180501179_e20223180503552_c20223180508262.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-DMWVM1-M6C08_G18_s20223552050271_e20223552050328_c20223552122197.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-DMWVM/2022/355/20/OR_ABI-L2-DMWVM1-M6C08_G18_s20223552050271_e20223552050328_c20223552122197.nc"
+
+    response = client.post(url = "/getfileUrl",  json = {"file_name": 'OR_ABI-L2-ACMC-M6_G18_s20222800931164_e20222800933537_c20222800934574.nc'})
+    assert response.status_code == 200
+    url = response.json()['message']
+    assert url == "https://noaa-goes18.s3.amazonaws.com/ABI-L2-ACMC/2022/280/09/OR_ABI-L2-ACMC-M6_G18_s20222800931164_e20222800933537_c20222800934574.nc"
+    
+    
 # def test_goes18_link_generation():
     
 #     assert 'https://noaa-goes18.s3.amazonaws.com/ABI-L1b-RadC/2023/002/01/OR_ABI-L1b-RadC-M6C01_G18_s20230020101172_e20230020103548_c20230020103594.nc'== main_goes18.create_url('ABI-L1b-RadC','2023','002','01','OR_ABI-L1b-RadC-M6C01_G18_s20230020101172_e20230020103548_c20230020103594.nc')
